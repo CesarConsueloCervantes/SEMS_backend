800a363197e3bfddfb98e93e6623aa06a391cfa5
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-06-11 23:19:39 -0600

    inicio del proyecto :tada:

---
+++ database/migrations/Archives/Metadata/2026_06_06_005518_create_metadata_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('metadata', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->String('uuid');  //Uuid
+            $table->String('rfc_emisor');  //RfcEmisor
+            $table->String('nombre_emisor');  //NombreEmisor
+            $table->String('rfc_receptor');  //RfcReceptor
+            $table->String('nombre_receptor');  //NombreReceptor
+            $table->String('pac_certifico');  //PacCertifico
+            $table->String('fecha_emision');  //FechaEmision
+            $table->String('fecha_certificacion_sat');  //FechaCertificacionSat
+            $table->String('monto');  //Monto
+            $table->String('iva');  //IVA
+            $table->String('sub_total');  //SubTotal
+            $table->String('efecto_comprobante');  //EfectoComprobante
+            $table->boolean('estatus');  //Estatus
+            $table->String('fecha_cancelacion');  //FechaCancelacion 
+            $table->String('state');  //estatus
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('metadata');
+    }
+};
---
+++ database/migrations/Archives/Processed/2026_06_05_235043_create_archives_processed_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('archives_processed', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->string('archive_name');
+            $table->string('archive_hash');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('archives_processed');
+    }
+};

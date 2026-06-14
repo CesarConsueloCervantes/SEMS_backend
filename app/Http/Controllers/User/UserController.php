79444f823790cbd7f4f80f7fcd4e852e4c0b2eb3
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-06-14 17:12:17 -0600

    feat :sparkles: (Controllers): se crearon los controllers de los archivos procesados, de los metadatos y del usuario

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+
+class UserController extends Controller
+{
+    public function index(): JsonResponse
+    {
+
+        return response()->json();
+    }
+
+    public function show(): JsonResponse
+    {
+
+        return response()->json();
+    }
+
+    public function store()
+    {
+
+        return;
+    }
+
+    public function update(): JsonResponse
+    {
+
+        return response()->json();
+    }
+
+    public function delete(): JsonResponse
+    {
+
+        return response()->json();
+    }
+}

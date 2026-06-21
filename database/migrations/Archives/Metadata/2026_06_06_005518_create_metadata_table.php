3470f50f24396783e60d36672febc3f46b255403
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-06-20 22:15:44 -0600

    fix :bug: (create_metadata_table): se agrego la llave foranea para los archivos prosesados y se cambiaron siertos atributos

--- database/migrations/Archives/Metadata/2026_06_06_005518_create_metadata_table.php
+++ database/migrations/Archives/Metadata/2026_06_06_005518_create_metadata_table.php
@@ -14,14 +14,15 @@ return new class extends Migration
         Schema::create('metadata', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id');
-            $table->String('uuid');  //Uuid
+            $table->foreignId('archive_prossesed_id');
+            $table->uuid('uuid')->unique();  //Uuid
             $table->String('rfc_emisor');  //RfcEmisor
             $table->String('nombre_emisor');  //NombreEmisor
             $table->String('rfc_receptor');  //RfcReceptor
             $table->String('nombre_receptor');  //NombreReceptor
             $table->String('pac_certifico');  //PacCertifico
-            $table->String('fecha_emision');  //FechaEmision
-            $table->String('fecha_certificacion_sat');  //FechaCertificacionSat
+            $table->dateTime('fecha_emision');  //FechaEmision
+            $table->dateTime('fecha_certificacion_sat');  //FechaCertificacionSat
             $table->String('monto');  //Monto
             $table->String('iva');  //IVA
             $table->String('sub_total');  //SubTotal

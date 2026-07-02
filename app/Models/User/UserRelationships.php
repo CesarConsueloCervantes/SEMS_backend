3e0eb9c8cac7b4bc212b06cad817912bcb381c68
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-01 22:26:08 -0600

    feat :sparkles: (UserRelationships.php): se crearon las relaciones para el modelo user

--- app/Models/User/UserRelationships.php
+++ app/Models/User/UserRelationships.php
@@ -1,4 +1,37 @@
 <?php
 
+namespace App\Models\User;
 
-//el trait de las relaciones del usuario
+use App\Models\Archives\ArchivesProssesed\ArchivesProssesed;
+use App\Models\Archives\Metadata\Metadata;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
+
+/**
+ * @mixin \Illuminate\Database\Eloquent\Model
+ */
+trait UserRelationships
+{
+    /**
+     * archives prossesed by this user.
+     */
+    public function archivesProcessed(): HasMany
+    {
+        return $this->hasMany(ArchivesProssesed::class, 'user_id');
+    }
+
+    /**
+     * get metadata for the user.
+     */
+    public function metadata(): HasManyThrough
+    {
+        return $this->hasManyThrough(
+            Metadata::class,
+            ArchivesProssesed::class,
+            'user_id',               // Foreign key in archives_processed table
+            'archive_prossesed_id',  // Foreign key in metadata table
+            'id',                    // Local key in users table
+            'id'                     // Local key in archives_processed table
+        );
+    }
+}

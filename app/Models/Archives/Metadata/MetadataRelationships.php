43c84f7c2111c1a06a8e66f73552767f8d7eb8e5
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-01 22:18:57 -0600

    feat :sparkles: (MetadataRelationships.php): se crearon las relaciones para el modelo metadata

--- app/Models/Archives/Metadata/MetadataRelationships.php
+++ app/Models/Archives/Metadata/MetadataRelationships.php
@@ -1,3 +1,29 @@
 <?php
 
-//el trait de las relaciones de los metadatos
+namespace App\Models\Archives\Metadata;
+
+use App\Models\User\User;
+use App\Models\Archives\ArchivesProssesed\ArchivesProssesed;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * @mixin \Illuminate\Database\Eloquent\Model
+ */
+trait MetadataRelationships
+{
+    /**
+     * metadata belongs to user.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * metadata belongs to archive prossesed.
+     */
+    public function archiveProcessed(): BelongsTo
+    {
+        return $this->belongsTo(ArchivesProssesed::class);
+    }
+}

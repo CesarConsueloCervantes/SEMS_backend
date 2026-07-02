075d16fc0632749ac4cca0d17eb61e9c33c969ea
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-01 22:47:22 -0600

    feat :sparkles: (UserActions.php): se creo la funcion login

--- app/Models/User/UserActions.php
+++ app/Models/User/UserActions.php
@@ -1,5 +1,17 @@
 <?php
 
-//el trait de las acciones del usuario
+namespace App\Models\User;
 
-//el login
+trait UserActions
+{
+    public function login(bool $create_token = true): array
+    {
+        $access_token = $create_token ? $this->createToken('login')->accessToken : null;
+        $user = $this->only([
+            'id',
+            'name',
+            'email',
+        ]);
+        return ['access_token' => $access_token, 'user' => $user];
+    }
+}

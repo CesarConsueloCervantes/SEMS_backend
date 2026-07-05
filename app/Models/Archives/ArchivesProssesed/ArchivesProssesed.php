3a43924489ede2cf62c1e05ded5aa74600c5bd77
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-05 16:16:12 -0600

    feat :sparkles: (ArchivesProssesed.php): se creo el modelo de los archivos prosesados

--- app/Models/Archives/ArchivesProssesed/ArchivesProssesed.php
+++ app/Models/Archives/ArchivesProssesed/ArchivesProssesed.php
@@ -1,10 +1,22 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Archives\ArchivesProssesed;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ArchivesProssesed extends Model
 {
-    //
+    use ArchivesProssesedRelationships, ArchivesProssesedAccessors, ArchivesProssesedScopes;
+    use HasFactory;
+
+    protected $table = 'archives_processed';
+
+    protected $fillable = [
+        'user_id',
+        'archive_name',
+        'archive_hash',
+        'archive_path',
+        'archive_size_bytes',
+    ];
 }

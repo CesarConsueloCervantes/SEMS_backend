1f7a8f34b79de6aaa4270c8fb3804c4c2d2a7d26
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-07 12:27:06 -0600

    feat :sparkles: (ArchivesProssesed.php): se agrego el constructor del repositorio

--- app/Http/Controllers/Archives/ArchivesProssesedController.php
+++ app/Http/Controllers/Archives/ArchivesProssesedController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Archives\ArchivesProssesed\ArchivesProssesed;
+use App\Repositories\Archives\ArchivesProssesedRepositorie;
 use Illuminate\Http\JsonResponse;
 
 class ArchivesProssesedController extends Controller
 {
+    private ArchivesProssesedRepositorie $repository;
+
+    public function __construct(ArchivesProssesedRepositorie $repository)
+    {
+        $this->$repository = $repository;
+    }
+
     public function index(): JsonResponse
     {
-
+        
         return response()->json();
     }
 

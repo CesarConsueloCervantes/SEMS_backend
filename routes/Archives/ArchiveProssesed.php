c57a24ba91786b23087280475401d980a6a56e79
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-09 22:10:54 -0600

    feat :sparkles: (Archivesprossesed.php): se agrego la ruta de la consulta del archivo

--- routes/Archives/ArchiveProssesed.php
+++ routes/Archives/ArchiveProssesed.php
@@ -1,3 +1,8 @@
 <?php
 
-// rutas de los archivos prosesados
+use App\Http\Controllers\Archives\ArchivesProssesedController;
+use Illuminate\Support\Facades\Route;
+
+Route::controller(ArchivesProssesedController::class)->group(function () {
+    Route::post('consult_archive_info', 'consultHashAndNameExists');
+});

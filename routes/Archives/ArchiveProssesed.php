7b1a383939aff9b9e7cf05ebb81b320a5bd07a0e
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-13 11:55:40 -0600

    feat :sparkles: (ArchiveProssesed.php): se agregaron las rutas restantes

--- routes/Archives/ArchiveProssesed.php
+++ routes/Archives/ArchiveProssesed.php
@@ -4,5 +4,10 @@ use App\Http\Controllers\Archives\ArchivesProssesedController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(ArchivesProssesedController::class)->group(function () {
+    Route::get('archive/index', 'index');
+    Route::get('archive/show', 'show');
+    Route::post('archive/store', 'store');
+    Route::put('archive/update', 'update');
+    Route::delete('archive/delete', 'delete');
     Route::post('consult_archive_info', 'consultHashAndNameExists');
 });

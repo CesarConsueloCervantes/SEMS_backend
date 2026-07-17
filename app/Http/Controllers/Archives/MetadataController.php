cba856c2d1574c6240f8dd687baf462b7674a965
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-16 22:58:53 -0600

    feat :sparkles: (Metadatacontroller.php): ahora el indexByUser regresa toda la informacion requerida

--- app/Http/Controllers/Archives/MetadataController.php
+++ app/Http/Controllers/Archives/MetadataController.php
@@ -1,15 +1,40 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Archives;
 
+use App\Http\Controllers\Controller;
 use App\Models\User\User;
+use App\Repositories\Archives\MetadataRepositorie;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 
 class MetadataController extends Controller
 {
+    private MetadataRepositorie $repository;
+
+    public function __construct(MetadataRepositorie $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Display a listing of all metadata records belonging to the specified user,
+     * ordered and filtered using the generic repository index method.
+     *
+     * @param User $user
+     * @return JsonResponse
+     */
     public function indexByUser(User $user): JsonResponse
     {
+        $first = request('first', false);
+        $rows = request('rows', false);
+        $orderBy = request('orderBy', false);
+        $ascending = request('ascending', '1');
+        $filters = json_decode(request('filters', '{}'), true);
+        $columns = request()->has('columns') ? json_decode(request('columns')) : array_keys($filters);
 
-        return response()->json();
+        $result = $this->repository->index($first, $rows, $orderBy, $ascending, $filters, $columns, $user->metadata());
+
+        return response()->json($result, Response::HTTP_OK);
     }
 }

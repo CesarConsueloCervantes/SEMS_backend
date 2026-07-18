994b59ce658ab83a9c4549dc950b0e481d74332c
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-17 23:41:58 -0600

    feat :sparkles: (api.php): se creo la ruta pra el login

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
+
+Route::post('login', [UserController::class, 'oauthLogin'])->name('login');
 
 Route::group([] ,function () {
     include_routes_dir(__DIR__. '/Archives');

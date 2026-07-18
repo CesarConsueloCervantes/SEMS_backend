44a3d951613f80faef53ff1e7e54ed7866172a5e
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-17 23:39:42 -0600

    feat :sparkles: (UserActions): se creo el metodo login

--- app/Models/User/UserActions.php
+++ app/Models/User/UserActions.php
@@ -4,6 +4,12 @@ namespace App\Models\User;
 
 trait UserActions
 {
+    /**
+     * Log the user in and generate an access token.
+     * 
+     * @param bool $create_token
+     * @return array
+     */
     public function login(bool $create_token = true): array
     {
         $access_token = $create_token ? $this->createToken('login')->accessToken : null;

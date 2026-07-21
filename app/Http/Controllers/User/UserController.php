35d5141262becd24bf890199170c43ba1cd3d6e4
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-20 19:41:38 -0600

    feat :sparkles: (AuthController.php): se separo toda la logica de la authentificacion

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,14 +3,8 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\User\OauthLoginRequest;
-use App\Http\Requests\User\RegisterRequest;
-use App\Models\User\User;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
@@ -42,65 +36,5 @@ class UserController extends Controller
     {
 
         return response()->json();
-    }
-
-    /**
-     * Authenticate the user and generate an access token.
-     * 
-     * @param OauthLoginRequest $request
-     * @return JsonResponse
-     */
-    public function oauthLogin(OauthLoginRequest $request): JsonResponse
-    {
-        if (! Auth::attempt($request->only('email', 'password'))) {
-            return response()->json([
-                'message' => 'Las credenciales no coinciden con nuestros registros.'
-            ], 422);
-        }
-
-        /** @var \App\Models\User\User $user */
-        $user = Auth::user();
-
-        $user->tokens()->update([
-            'revoked' => true,
-        ]);
-
-        return response()->json(
-            $user->login()
-        );
-    }
-
-    /**
-     * Register a new user, store in the database, and return an access token.
-     * 
-     * @param RegisterRequest $request
-     * @return JsonResponse
-     */
-    public function register(RegisterRequest $request): JsonResponse
-    {
-        // Crea el nuevo registro del usuario
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
-
-        // Retorna la respuesta de inicio de sesión con el token de acceso
-        return response()->json($user->login(), Response::HTTP_CREATED);
-    }
-
-    /**
-     * Revoke token and logout user.
-     * 
-     * @param Request $request
-     * @return JsonResponse
-     */
-    public function logout(Request $request): JsonResponse
-    {
-        $request->user()->token()->revoke();
-
-        return response()->json([
-            'message' => 'Sesión cerrada correctamente.'
-        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,12 @@
 <?php
 
-use App\Http\Controllers\User\UserController;
+use App\Http\Controllers\System\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('login', [UserController::class, 'oauthLogin'])->name('login');
-Route::post('register', [UserController::class, 'register'])->name('register');
+Route::post('login', [AuthController::class, 'oauthLogin'])->name('login');
+Route::post('register', [AuthController::class, 'register'])->name('register');
 
 Route::middleware('auth:api')->group(function () {
-    Route::post('logout', [UserController::class, 'logout'])->name('logout');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     include_routes_dir(__DIR__. '/Archives');
 });

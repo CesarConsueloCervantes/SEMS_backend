816a7097d3bce4309d9dd3ca1bbda725cc52a823
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-20 19:02:36 -0600

    feat :sparkles: (api.php): se crearon las rutas del register, logout y se configuro el auth:api

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use App\Http\Controllers\User\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [UserController::class, 'oauthLogin'])->name('login');
+Route::post('register', [UserController::class, 'register'])->name('register');
+Route::post('logout', [UserController::class, 'logout'])->middleware('auth:api')->name('logout');
 
-Route::group([] ,function () {
+Route::middleware('auth:api')->group(function () {
     include_routes_dir(__DIR__. '/Archives');
 });

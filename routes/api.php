ad3ae75cd513e2e839c36f3f6da63318560a3c81
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-20 19:36:00 -0600

    fix :bug: (api.php): se protegio la ruta logout

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('login', [UserController::class, 'oauthLogin'])->name('login');
 Route::post('register', [UserController::class, 'register'])->name('register');
-Route::post('logout', [UserController::class, 'logout'])->middleware('auth:api')->name('logout');
 
 Route::middleware('auth:api')->group(function () {
+    Route::post('logout', [UserController::class, 'logout'])->name('logout');
     include_routes_dir(__DIR__. '/Archives');
 });

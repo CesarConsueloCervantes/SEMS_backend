7ba0c3f563ca46c8fc63c5e7bbf7c9b321757b5a
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-20 19:27:38 -0600

    fix :bug: (ArchiveProssesed.php): se cambio una ruta

--- routes/Archives/ArchiveProssesed.php
+++ routes/Archives/ArchiveProssesed.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Archives\ArchivesProssesedController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(ArchivesProssesedController::class)->group(function () {
-    Route::get('archives-prossesed/{user}', 'indexByUser');
+    Route::get('user/archives-prossesed/{user}', 'indexByUser');
     Route::get('archives-prossesed/{archives_prossesed}', 'show');
     Route::post('archives-prossesed', 'store');
     Route::put('archives-prossesed/{archives_prossesed}', 'update');

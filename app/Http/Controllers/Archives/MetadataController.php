78930af3267336b08e837ccefe4ead5e8dd333fa
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-24 19:57:58 -0600

    fix :bug: (MetadataController.php): ahora se obtiene el usuario authenticado

--- app/Http/Controllers/Archives/MetadataController.php
+++ app/Http/Controllers/Archives/MetadataController.php
@@ -3,10 +3,10 @@
 namespace App\Http\Controllers\Archives;
 
 use App\Http\Controllers\Controller;
-use App\Models\User\User;
 use App\Repositories\Archives\MetadataRepositorie;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 
 class MetadataController extends Controller
 {
@@ -21,11 +21,13 @@ class MetadataController extends Controller
      * Display a listing of all metadata records belonging to the specified user,
      * ordered and filtered using the generic repository index method.
      *
-     * @param User $user
      * @return JsonResponse
      */
-    public function indexByUser(User $user): JsonResponse
+    public function indexByUser(): JsonResponse
     {
+        /** @var \App\Models\User\User $user */
+        $user = Auth::user();
+
         $first = request('first', false);
         $rows = request('rows', false);
         $orderBy = request('orderBy', false);

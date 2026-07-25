1072dd8b6c5ad894ee09326e7e6a51ddf56d8b34
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-07-24 19:58:29 -0600

    fix :bug: (routes): se elimino la necesidad del parametro user

--- routes/Archives/metadata.php
+++ routes/Archives/metadata.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\Archives\MetadataController;
 use Illuminate\Support\Facades\Route;
 
 Route::controller(MetadataController::class)->group(function () {
-    Route::get('metadata/{user}', 'indexByUser');
+    Route::get('metadata/', 'indexByUser');
 });

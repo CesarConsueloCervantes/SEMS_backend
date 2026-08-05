2cff87a305743514b7dd8885b6bf9ddab2718424
cesar.sergio <cesar.consuelo.cervantes@gmail.com>
2026-08-04 19:23:34 -0600

    feat :sparkels: (metadta.php): se agrego la ruta para las exportaciones

--- routes/Archives/metadata.php
+++ routes/Archives/metadata.php
@@ -6,4 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::controller(MetadataController::class)->group(function () {
     Route::get('metadata/', 'indexByUser');
     Route::get('metadata/options', 'filtersOptions');
+    Route::get('metadata/export', 'export');
 });
